1ad5ca51e256da826f1782a15c271456b0bb6449
Ali Susanto <alisusanto.main@gmail.com>
2022-11-30 21:29:11 +0700

    add widget circular slider

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kalori/constants.dart';
 import 'package:kalori/service/auth_service.dart';
 import 'package:provider/provider.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
       return 'Selamat pagi';
     } else if ((timeNow > 12) && (timeNow <= 16)) {
       return 'Selamat siang';
-    } else if ((timeNow > 16) && (timeNow < 20)) {
+    } else if ((timeNow > 16) && (timeNow < 18)) {
       return 'Selamat sore';
     } else {
       return 'Selamat malam';
@@ -43,38 +45,42 @@ class _HomeScreenState extends State<HomeScreen> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          greetingMessage(),
-                          style: const TextStyle(
-                              color: Colors.grey, fontWeight: FontWeight.bold),
-                        ),
+                        Text(greetingMessage(),
+                            style: const TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 24)),
                         const SizedBox(
-                          height: 10,
+                          height: 5,
                         ),
                         Text(
                           '${viewModel.user.name}',
                           style: const TextStyle(
-                              fontSize: 28,
+                              fontSize: 16,
                               fontWeight: FontWeight.bold,
-                              color: Colors.black),
+                              color: Colors.black54),
                         ),
                       ],
                     ),
                     Row(
-                      children: const [
+                      children: [
                         Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: CircleAvatar(
-                            backgroundColor: Colors.blueAccent,
-                            child: ClipOval(
-                              child: Icon(
-                                Icons.person,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-
-                              //45:00 min
-                            ),
+                          padding: const EdgeInsets.all(10.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(200),
+                            child: viewModel.user.photoUrl! == "noimage"
+                                ? Image.asset(
+                                    "assets/logo/noimage.png",
+                                    height: 50,
+                                    width: 50,
+                                    fit: BoxFit.cover,
+                                  )
+                                : Image.network(
+                                    viewModel.user.photoUrl!,
+                                    width: 50,
+                                    height: 50,
+                                    fit: BoxFit.cover,
+                                  ),
                           ),
                         ),
                       ],
@@ -88,7 +94,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Container(
                 width: size.width,
                 padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
                 decoration: BoxDecoration(
                     color: Colors.blueAccent,
                     borderRadius: BorderRadius.circular(12)),
@@ -96,17 +102,84 @@ class _HomeScreenState extends State<HomeScreen> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text('129 Calories'),
-                        Text('129 Calories'),
-                      ],
+                    const Text(
+                      'kalori hari ini',
+                      style: txtGeneralBoldWhite,
                     ),
                     const SizedBox(
-                      height: 15,
+                      height: 10,
                     ),
-                    const Text('129 Calories')
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text('50',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 46,
+                              color: Colors.white,
+                            )),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text('Kalori',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 26,
+                              color: Colors.white,
+                            )),
+                      ],
+                    ),
+                    const Divider(color: Colors.white),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SleekCircularSlider(
+                          initialValue: 24,
+                          appearance: CircularSliderAppearance(
+                            size: size.width * 0.35,
+                            customColors: CustomSliderColors(
+                                trackColor: Colors.greenAccent,
+                                progressBarColors: [
+                                  Colors.lightGreen,
+                                  Colors.amberAccent
+                                ],
+                                shadowMaxOpacity: 20.0),
+                            infoProperties: InfoProperties(
+                                topLabelText: 'Karbohidrat',
+                                topLabelStyle: txtLabelCircularSlider,
+                                modifier: (double value) {
+                                  final gram = value.toDouble();
+                                  return '$gram g';
+                                }),
+                          ),
+                        ),
+                        SleekCircularSlider(
+                          initialValue: 44.50,
+                          min: 0,
+                          max: 100,
+                          appearance: CircularSliderAppearance(
+                            size: size.width * 0.35,
+                            customColors: CustomSliderColors(
+                                trackColor: Colors.orangeAccent,
+                                progressBarColors: [
+                                  Colors.lightGreen,
+                                  const Color(0xffFFBF00)
+                                ],
+                                shadowMaxOpacity: 20.0),
+                            infoProperties: InfoProperties(
+                                topLabelText: 'Protein',
+                                topLabelStyle: txtLabelCircularSlider,
+                                modifier: (double value) {
+                                  final gram = value.toDouble();
+                                  return '$gram g';
+                                }),
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               )
