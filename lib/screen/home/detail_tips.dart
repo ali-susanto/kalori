17cf4d0bf3ba6bf7948e38658e4d99e593eb9b64
Ali Susanto <alisusanto.main@gmail.com>
2022-12-09 20:47:24 +0700

    add detail article

--- lib/screen/home/detail_tips.dart
+++ lib/screen/home/detail_tips.dart
@@ -21,21 +21,57 @@ class DetailTipsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(
-                Icons.arrow_back_ios_outlined,
-                color: kBlackColor,
-              )),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: const Text(
-            'Detail',
-            style: TextStyle(color: kBlackColor),
-          )),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(children: [
+              Image.network(image),
+              Padding(
+                padding: const EdgeInsets.only(top: 32, left: 10),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Card(
+                    color: Colors.black87.withOpacity(0.4),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 4.5, horizontal: 8),
+                      child: Icon(Icons.arrow_back_ios_outlined,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+              child: Column(
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    headline,
+                    style: Styles.txtRegular,
+                    textAlign: TextAlign.justify,
+                  ),
+                  Text(
+                    content,
+                    style: Styles.txtRegular,
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
