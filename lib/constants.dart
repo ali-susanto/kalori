cd3ff32bb41da1324336eb8778ce9cb4039e00fb
Ali Susanto <alisusanto.main@gmail.com>
2022-12-18 21:15:06 +0700

    add: new textstyle

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryColor = Color(0xFF242072);
+const kSecondryColor = Color(0xFF383786);
+const kTertiaryColor = Color(0xFFb3b5cb);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
 
 const kPrimaryBlue = Color(0xFF4163b1);
@@ -23,49 +25,105 @@ class Styles {
   static const TextStyle txtRegular = TextStyle(fontSize: 14);
   static const TextStyle txtLabelRegularCircularSlider =
       TextStyle(fontSize: 14, fontWeight: FontWeight.w600);
+  static const TextStyle txtMainLabel =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 28, color: Colors.white);
+  static const TextStyle txtRegularWhite =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 16, color: Colors.white);
+  static const TextStyle txtLabelCardWhite =
+      TextStyle(fontWeight: FontWeight.w600, fontSize: 12, color: Colors.white);
 }
 
 class DataObject {
   List valueObject = [
     {
       "nama": "Bakso",
-      "kandungan": {"karbohidrat": "16.4", "protein": "5.3","lemak":"3", "kalori": "114  "}
+      "kandungan": {
+        "karbohidrat": "16.4",
+        "protein": "5.3",
+        "lemak": "3",
+        "kalori": "114  "
+      }
     },
     {
       "nama": "Gado-gado",
-      "kandungan": {"karbohidrat": "21", "protein": "6.1","lemak":"3.2", "kalori": "137"}
+      "kandungan": {
+        "karbohidrat": "21",
+        "protein": "6.1",
+        "lemak": "3.2",
+        "kalori": "137"
+      }
     },
     {
       "nama": "Mie ayam",
-      "kandungan": {"karbohidrat": "10.5", "protein": "6.2","lemak":"3.9", "kalori": "102"}
+      "kandungan": {
+        "karbohidrat": "10.5",
+        "protein": "6.2",
+        "lemak": "3.9",
+        "kalori": "102"
+      }
     },
     {
       "nama": "Martabak Telor",
-      "kandungan": {"karbohidrat": "45", "protein": "5.1","lemak":"8.6", "kalori": "278"}
+      "kandungan": {
+        "karbohidrat": "45",
+        "protein": "5.1",
+        "lemak": "8.6",
+        "kalori": "278"
+      }
     },
     {
       "nama": "Ketoprak",
-      "kandungan": {"karbohidrat": "13", "protein": "7.9","lemak":"7.7", "kalori": "153"}
+      "kandungan": {
+        "karbohidrat": "13",
+        "protein": "7.9",
+        "lemak": "7.7",
+        "kalori": "153"
+      }
     },
     {
       "nama": "Rendang",
-      "kandungan": {"karbohidrat": "7.8", "protein": "22.6","lemak":"7.9", "kalori": "193"}
+      "kandungan": {
+        "karbohidrat": "7.8",
+        "protein": "22.6",
+        "lemak": "7.9",
+        "kalori": "193"
+      }
     },
     {
       "nama": "Soto Betawi",
-      "kandungan": {"karbohidrat": "11.5", "protein": "2.5","lemak":"8.8", "kalori": "135"}
+      "kandungan": {
+        "karbohidrat": "11.5",
+        "protein": "2.5",
+        "lemak": "8.8",
+        "kalori": "135"
+      }
     },
     {
       "nama": "Gulai Kambing",
-      "kandungan": {"karbohidrat": "6.2", "protein": "4.2","lemak":"9.4", "kalori": "126"}
+      "kandungan": {
+        "karbohidrat": "6.2",
+        "protein": "4.2",
+        "lemak": "9.4",
+        "kalori": "126"
+      }
     },
     {
       "nama": "Semur Jengkol",
-      "kandungan": {"karbohidrat": "29.1", "protein": "6", "lemak":"10","kalori": "212"}
+      "kandungan": {
+        "karbohidrat": "29.1",
+        "protein": "6",
+        "lemak": "10",
+        "kalori": "212"
+      }
     },
     {
       "nama": "Sate",
-      "kandungan": {"karbohidrat": "14.82", "protein": "19.54","lemak":"14.82", "kalori": "225"}
+      "kandungan": {
+        "karbohidrat": "14.82",
+        "protein": "19.54",
+        "lemak": "14.82",
+        "kalori": "225"
+      }
     },
   ];
 }
