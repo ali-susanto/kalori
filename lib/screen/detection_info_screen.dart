debcc54b985296c1552aad7a3dd3158843f73e51
Ali Susanto <alisusanto.main@gmail.com>
2022-12-24 16:15:10 +0700

    add: title & change appbar

--- lib/screen/detection_info_screen.dart
+++ lib/screen/detection_info_screen.dart
@@ -10,20 +10,29 @@ class DetectionInfoScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Info',
-          style: TextStyle(color: Colors.black),
+          // style: TextStyle(color: Colors.black),
         ),
-        backgroundColor: Colors.grey.withOpacity(0.01),
+        // backgroundColor: Colors.grey.withOpacity(0.01),
         elevation: 0,
-        iconTheme: const IconThemeData(color: Colors.black),
+        // iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 13),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const Center(
+              child: Text(
+                'Informasi Tentang Deteksi Makanan',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
             const Text(
               'Data makanan yang bisa terdeteksi : ',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
             ),
             const SizedBox(
               height: 5,
@@ -31,8 +40,11 @@ class DetectionInfoScreen extends StatelessWidget {
             ...List.generate(DataObject().valueObject.length, (index) {
               return Padding(
                 padding: const EdgeInsets.only(left: 16),
-                child: Text('${index + 1}.   ' +
-                    DataObject().valueObject[index]['nama']),
+                child: Text(
+                  '${index + 1}.   ' + DataObject().valueObject[index]['nama'],
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 14),
+                ),
               );
             }),
             const SizedBox(
@@ -71,7 +83,7 @@ class DetectionInfoScreen extends StatelessWidget {
                 ),
                 Flexible(
                   child: Text(
-                    'Sumber untuk data gizi yaitu pada website www.panganku.org milik Kementrian Kesehatan Republik Indonesia.',
+                    'Sumber untuk data gizi yaitu pada website www.panganku.org milik Kementerian Kesehatan Republik Indonesia.',
                     textAlign: TextAlign.start,
                   ),
                 ),
