6dfecd9622429bb8454c3e22f8bb605710d8edef
Ali Susanto <alisusanto.main@gmail.com>
2023-01-07 22:46:00 +0700

    add: unit testing

---
+++ test/unit_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kalori/service/tips_api.dart';
+import 'package:tflite/tflite.dart';
+
+void main() {
+  group('Unit Testing =>', () {
+    test("Deteksi makanan", () async {
+      try {
+        await Tflite.loadModel(
+            model: "assets/models/model_unquant.tflite",
+            labels: "assets/models/labels.txt");
+
+        var outputData = await Tflite.runModelOnImage(
+            path: 'assets/images/bakso.jpg',
+            imageMean: 127.5,
+            imageStd: 127.5,
+            threshold: 0.5);
+        expect(outputData!.first['label'], 'Bakso');
+      } catch (e) {}
+    });
+
+    test('Data artikel', () async {
+      var data = await TipsApi().getTipsData();
+      expect(data.isNotEmpty, true);
+    });
+  });
+}
